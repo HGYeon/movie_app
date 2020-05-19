ea67220412ec00c11dae5aaddc1c391f98346313
HGYeon <hanjackpot@gmail.com>
2020-05-19 14:11:58 +0900

    Updates

---
+++ precache-manifest.0adc1f2be030e0333e1a01602230cae9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5cf68a118c5cae5a677b4a0198fe91f",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "d00ae5698dbe3b1c8891",
+    "url": "/movie_app/static/css/main.54e5070a.chunk.css"
+  },
+  {
+    "revision": "964546851f2431c5d268",
+    "url": "/movie_app/static/js/2.2d3cfcc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.2d3cfcc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d00ae5698dbe3b1c8891",
+    "url": "/movie_app/static/js/main.be1ef5d0.chunk.js"
+  },
+  {
+    "revision": "ffbd4de4896f1a599175",
+    "url": "/movie_app/static/js/runtime-main.0186e20d.js"
+  }
+]);
